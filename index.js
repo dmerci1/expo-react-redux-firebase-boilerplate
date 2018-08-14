229dcd2e0fa745b1a2e5c937f022c99af3e6ba3f
dmerci1 <danmercia@gmail.com>
2018-08-14 15:41:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import App from './src/App';
+
+class Index extends Component {
+  render() {
+    return(
+      <App/>
+    );
+  }
+}
+
+export default Index;
